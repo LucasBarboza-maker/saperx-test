e384a3a8811a7850513dd36b0cf6b13bf765439c
LucasBarboza-maker <61573957+LucasBarboza-maker@users.noreply.github.com>
2023-03-21 12:52:15 -0300

    Chore: Adding my migrations and removing standard migrations

--- database/migrations/2023_03_21_144618_phonebook.php
+++ database/migrations/2023_03_21_144618_phonebook.php
@@ -4,22 +4,21 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePersonalAccessTokensTable extends Migration
+class Phonebook extends Migration
 {
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up()
+   public function up()
     {
-        Schema::create('personal_access_tokens', function (Blueprint $table) {
+        Schema::create('contact', function (Blueprint $table) {
             $table->id();
-            $table->morphs('tokenable');
             $table->string('name');
-            $table->string('token', 64)->unique();
-            $table->text('abilities')->nullable();
-            $table->timestamp('last_used_at')->nullable();
+            $table->string('email')->unique();
+            $table->timestamp('birth');
+            $table->string('cpf')->unique()->regex('/^[0-9]+$/');
             $table->timestamps();
         });
     }
@@ -31,6 +30,6 @@ class CreatePersonalAccessTokensTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('personal_access_tokens');
+        Schema::dropIfExists('users');
     }
 }
---
+++ database/migrations/2023_03_21_151423_phone_numbers.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class PhoneNumbers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('phone', function (Blueprint $table) {
+            $table->id();
+            $table->string('phone_number', 14)->nullable()->unique()->digits()->regex('/^\+?\d{11,}$/');
+            $table->unsignedBigInteger('contact_id');
+
+            $table->foreign('contact_id')->references('id')->on('contact');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('phone');
+    }
+}
