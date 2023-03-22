3f528fd365304117dfca3ec98468f9b28b9c7395
LucasBarboza-maker <61573957+LucasBarboza-maker@users.noreply.github.com>
2023-03-21 23:55:49 -0300

    Chore: adding delete route to phone

--- app/Http/Controllers/PhoneController.php
+++ app/Http/Controllers/PhoneController.php
@@ -1,10 +1,21 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Phone;
+use Exception;
 use Illuminate\Http\Request;
 
 class PhoneController extends Controller
 {
-    //
+    
+    public function delete(Phone $phone){
+        try {
+            
+            $phone->delete();
+
+            return response()->json(['message' => 'Success!']);
+        } catch (Exception  $e) {
+            return response()->json(['message' => 'Error: ' . $e->getMessage()], 500);
+        }
+    }
 }
